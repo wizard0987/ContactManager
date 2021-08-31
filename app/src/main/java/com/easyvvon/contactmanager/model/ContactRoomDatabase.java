6b383bcae8d234db07aa900461c67d5860b787f2
leejiwon <leejiwon3737@gmail.com>
2021-08-31 13:44:22 +0900

    Model : 추상 클래스 RoomDatabase getContactDAO 내 추상 메서드 추가 (DAO를 통해 SQLite DB에 접속)

--- app/src/main/java/com/easyvvon/contactmanager/model/ContactRoomDatabase.java
+++ app/src/main/java/com/easyvvon/contactmanager/model/ContactRoomDatabase.java
@@ -1,4 +1,14 @@
 package com.easyvvon.contactmanager.model;
 
-public abstract class ContactRoomDatabase {
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.easyvvon.contactmanager.model.entity.Contact;
+
+// 이 프로젝트의 다른 부분에서 ContactsRoomDatabase 인스턴스를 만들고
+// 이 getContactDAO() 메서드를 호출하여 ContactDAO에서 선언한 모든 메서드를 호출하여 Room DB와 통신할 수 있다.
+@Database(entities = {Contact.class}, version = 1)
+public abstract class ContactRoomDatabase extends RoomDatabase {
+
+    public abstract ContactDAO getContactDAO();
 }
