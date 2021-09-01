000bdb0f6792a5d358560c784783c112f0b3a913
leejiwon <leejiwon3737@gmail.com>
2021-09-02 05:12:42 +0900

    View : ReycyclerView에 연락처 목록의 각 아이템 layout 설계 및 MainActivity, ContactAdapter 클래스 구현

--- app/src/main/java/com/easyvvon/contactmanager/view/MainActivity.java
+++ app/src/main/java/com/easyvvon/contactmanager/view/MainActivity.java
@@ -1,20 +1,84 @@
 package com.easyvvon.contactmanager.view;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.easyvvon.contactmanager.R;
+import com.easyvvon.contactmanager.model.entity.Contact;
+import com.easyvvon.contactmanager.view.adapter.ContactAdapter;
+import com.easyvvon.contactmanager.viewmodel.ContactViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ContactAdapter ContactAdapter;
+    private List<Contact> contactList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+    private ContactViewModel contactViewModel;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Contacts Manager");
+
+        // ViewModel 생성
+        contactViewModel = new ViewModelProvider.AndroidViewModelFactory(this.getApplication())
+                .create(ContactViewModel.class);
+        contactViewModel = new ViewModelProvider(this).get(ContactViewModel.class);
+
+        recyclerView = findViewById(R.id.recycler_view_contacts);
+
+        // 위 영화 정보들을 ContactAdapter에 전달
+        ContactAdapter = new ContactAdapter(this, contactList, MainActivity.this);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(ContactAdapter);
+
+        // ViewModel의 getContext() 메서드는 연락처 리스트를 실시간 데이터(LiveData)로 제공한다.
+        contactViewModel.getContactList().observe(this, new Observer<List<Contact>>() {
+            public void onChanged(List<Contact> contacts) {
+                contactList.clear();
+                contactList.addAll(contacts);
+                ContactAdapter.notifyDataSetChanged();
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addAndEditContacts(false, null, -1);
+            }
+
+        });
     }
 
     @Override
@@ -34,4 +98,91 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 
+    public void addAndEditContacts(final boolean isUpdate, final Contact contact, final int position) {
+        LayoutInflater layoutInflaterAndroid = LayoutInflater.from(getApplicationContext());
+        View view = layoutInflaterAndroid.inflate(R.layout.add_contact, null);
+
+        AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(MainActivity.this);
+        alertDialogBuilderUserInput.setView(view);
+
+        TextView contactTitle        = view.findViewById(R.id.tv_contactTitle);
+        final EditText newContact    = view.findViewById(R.id.ev_name);
+        final EditText contactEmail  = view.findViewById(R.id.ev_email);
+
+        contactTitle.setText(!isUpdate ? "Add New Contact" : "Edit Contact");
+
+        if (isUpdate && (contact != null)) {
+            newContact.setText(contact.getName());
+            contactEmail.setText(contact.getEmail());
+        }
+
+        alertDialogBuilderUserInput
+                .setCancelable(false)
+                .setPositiveButton( // '수정/확인' 역할 버튼 설정
+                        isUpdate ? "Update" : "Save",
+                        (dialogBox, id) -> {}
+                )
+                .setNegativeButton( // '삭제/취소' 역할 버튼 설정
+                        isUpdate ? "Delete" : "Cancel",
+                        (dialogBox, id) -> {
+                            if (isUpdate) {
+                                deleteContact(contact, position);
+                            } else {
+                                dialogBox.cancel();
+                            }
+                        }
+                );
+
+
+        final AlertDialog alertDialog = alertDialogBuilderUserInput.create();
+        alertDialog.show();
+
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 연락처 이름 작성란에 문자열이 없는 경우,
+                if (TextUtils.isEmpty(newContact.getText().toString())) {
+                    Toast.makeText(
+                            MainActivity.this,
+                            "Enter contact name!",
+                            Toast.LENGTH_SHORT
+                    ).show();
+                    return;
+                } else {
+                    // 연락처 이름 작성란에 문자열이 존재하는 경우 alertDialog 내리기(dismiss)
+                    alertDialog.dismiss();
+                }
+
+                if (isUpdate && (contact != null)) {
+                    updateContact(newContact.getText().toString(), contactEmail.getText().toString(), position);
+                } else {
+                    insertContact(newContact.getText().toString(), contactEmail.getText().toString());
+                }
+            }
+        });
+    }
+
+    private void deleteContact(final Contact contact, int position) {
+        contactViewModel.deleteContact(contact);
+    }
+
+    private void updateContact(String name, String email, int position) {
+
+        final Contact contact = contactList.get(position);
+
+        contact.setName(name);
+        contact.setEmail(email);
+
+        contactViewModel.updateContact(contact);
+
+    }
+
+    private void insertContact(final String name, final String email) {
+        contactViewModel.insertContact(name, email);
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        contactViewModel.clear();
+    }
 }
--- app/src/main/java/com/easyvvon/contactmanager/view/adapter/ContactAdapter.java
+++ app/src/main/java/com/easyvvon/contactmanager/view/adapter/ContactAdapter.java
@@ -1,11 +1,14 @@
 package com.easyvvon.contactmanager.view.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.easyvvon.contactmanager.R;
 import com.easyvvon.contactmanager.model.entity.Contact;
 import com.easyvvon.contactmanager.view.MainActivity;
 
@@ -13,28 +16,58 @@ import java.util.List;
 
 public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ContactViewHolder> {
 
+    private Context context;
+    private List<Contact> contactList;
+    private MainActivity mainActivity;
+
     public ContactAdapter(Context context, List<Contact> contacts, MainActivity mainActivity) {
+        this.context = context;
+        this.contactList = contacts;
+        this.mainActivity = mainActivity;
     }
 
     public class ContactViewHolder extends RecyclerView.ViewHolder {
 
+        public TextView tv_name;
+        public TextView tv_email;
+
         public ContactViewHolder(View view) {
             super(view);
+            tv_name = view.findViewById(R.id.tv_name);
+            tv_email = view.findViewById(R.id.tv_email);
         }
     }
 
     @Override
     public ContactViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+
+        View itemView = LayoutInflater.from(parent.getContext())
+                                    .inflate(R.layout.contact_list_item, parent, false);
+        return new ContactViewHolder(itemView);
+
     }
 
     @Override
     public void onBindViewHolder(ContactViewHolder holder, final int position) {
+
+        final Contact contact = contactList.get(position);
+
+        holder.tv_name.setText(contact.getName());
+        holder.tv_email.setText(contact.getEmail());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainActivity.addAndEditContacts(true, contact, position);
+            }
+        });
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+
+        return contactList.size();
     }
 
 }
